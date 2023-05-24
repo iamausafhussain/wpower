24e0e8bcc36ba7a3c8c5f5f6260407bda6dbd527
iamausafhussain <ausafhussain010@gmail.com>
2023-05-24 17:44:00 +0530

    added profile and cards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from 'src/shared/navbar/navbar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material/icon';
+import { HomeComponent } from 'src/components/home/home.component';
+import { UserprofileComponent } from 'src/widgets/userprofile/userprofile.component';
+import { RecommendeditemComponent } from 'src/widgets/recommendeditem/recommendeditem.component';
+import { RecommendedcardComponent } from 'src/widgets/recommendedcard/recommendedcard.component';
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent],
-  imports: [BrowserModule, BrowserAnimationsModule],
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    UserprofileComponent,
+    RecommendeditemComponent,
+    RecommendedcardComponent,
+  ],
+  imports: [BrowserModule, BrowserAnimationsModule, MatIconModule],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/widgets/recommendedcard/recommendedcard.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recommendedcard',
+  templateUrl: './recommendedcard.component.html',
+  styleUrls: ['./recommendedcard.component.css']
+})
+export class RecommendedcardComponent {
+
+}
