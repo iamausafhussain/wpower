d108abb07b5c06e81eabcafb36669e40853e6399
iamausafhussain <ausafhussain010@gmail.com>
2023-05-29 11:29:00 +0530

    added fixes

--- src/widgets/recommendedcard/recommendedcard.component.ts
+++ src/widgets/recommendedcard/recommendedcard.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-recommendedcard',
   templateUrl: './recommendedcard.component.html',
-  styleUrls: ['./recommendedcard.component.css']
+  styleUrls: ['./recommendedcard.component.css'],
 })
 export class RecommendedcardComponent {
-
+  @Input() descriptionProp: any;
+  @Input() imageProp: any;
 }
--- src/widgets/recommendeditem/recommendeditem.component.ts
+++ src/widgets/recommendeditem/recommendeditem.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-recommendeditem',
   templateUrl: './recommendeditem.component.html',
-  styleUrls: ['./recommendeditem.component.css']
+  styleUrls: ['./recommendeditem.component.css'],
 })
-export class RecommendeditemComponent {
-
-}
+export class RecommendeditemComponent {}
