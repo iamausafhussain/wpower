9dd33c086167c31566428ed5c66192e907247a2f
iamausafhussain <ausafhussain010@gmail.com>
2023-05-29 11:35:44 +0530

    added child styles

--- src/widgets/recommendedcard/recommendedcard.component.ts
+++ src/widgets/recommendedcard/recommendedcard.component.ts
@@ -8,4 +8,5 @@ import { Component, Input } from '@angular/core';
 export class RecommendedcardComponent {
   @Input() descriptionProp: any;
   @Input() imageProp: any;
+  @Input() backgroundProp: any;
 }
